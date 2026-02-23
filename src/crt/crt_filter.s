79a761eb3a1b8cda297bb34e0562ba9ed32271e7
Pierre-Saikali <pierre.saikali@gmail.com>
2026-02-23 12:13:34 -0500

    Implémentation problématique de crt

--- src/crt/crt_filter.s
+++ src/crt/crt_filter.s
@@ -35,7 +35,97 @@ crtFilter:
     movl    %esp, %ebp                  
 
     # TODO
-   
+    # Réserver 3 variables locales
+    addl $8, %esp
+    pushl %ebx
+    pushl %edi
+    pushl %esi
+
+    # Placer l'adresse de l'image dans eax et le facteur de Scanline dans ebx
+    movl 8(%ebp), %eax
+    movl 12(%ebp), %ebx
+
+    # Placer la hauteur de l'image dans la première variable locale
+    movl 4(%eax),%esi
+    movl %esi, -4(%ebp)
+
+    # Placer l'entité tableau de pointeurs de lignes de pixels dans ecx
+    movl 8(%eax), %ecx
+
+    iteration_hauteur:
+    # Placer la largeur de l'image dans la deuxième variable locale
+    movl (%eax), %edi
+    movl %edi, -8(%ebp)
+    # Placer l'indice de la ligne de pointeurs courante
+    pushl %eax
+    movl -4(%ebp), %eax
+    movl -4(%ecx, %eax, 4), %edx
+    popl %eax
+    
+    iteration_largeur:
+    # Placer le pointeur du pixel courant dans esi
+    pushl %eax
+    movl -8(%ebp), %eax
+    movl -4(%edx, %eax, 4), %esi
+    popl %eax
+
+    verification_Scanline:
+    cmpl %ebx, -4(%ebp)
+    jne appel_Phosphor
+
+    appel_Scanline:
+    # Appel de scanline
+    pushl %eax
+    pushl %edx
+    pushl %ecx
+    pushl less_color
+    pushl %esi
+    call applyScanline
+    addl $8, %esp
+    popl %ecx
+    popl %edx
+    popl %eax
+
+    appel_Phosphor:
+    # Calcul du subpixel
+    pushl %eax
+    pushl %edx
+    movl -8(%ebp), %eax
+    divl max_index
+    movl %edx, %edi
+    popl %edx
+    popl %eax
+    # Appel de phosphor
+    pushl %eax
+    pushl %edx
+    pushl %ecx
+    pushl %edi
+    pushl %esi
+    call applyPhosphor
+    addl $8, %esp
+    popl %ecx
+    popl %edx
+    popl %eax
+
+    incrementation_largeur:
+    # Note : tester dec si tout fonctionne
+    subl $1, -8(%ebp)
+    cmpl $0, -8(%ebp)
+    jne iteration_largeur
+
+    incrementation_hauteur:
+    subl $1, -4(%ebp)
+    cmpl $0, -4(%ebp)
+    jne iteration_hauteur
+
+
+    fin:
+
+    popl %esi
+    popl %edi
+    popl %ebx
+    # Libérer les variables locales
+    movl %ebp, %esp
     # epilogue
     leave 
     ret 
